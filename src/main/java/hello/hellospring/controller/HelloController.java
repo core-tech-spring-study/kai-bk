272614c2b967fa71715b15af31c0e50d7aa1a496
kai <kai.bk.study@gmail.com>
2022-07-31 00:00:04 +0900

    2nd commit

--- src/main/java/hello/hellospring/controller/HelloController.java
+++ src/main/java/hello/hellospring/controller/HelloController.java
@@ -3,6 +3,9 @@ package hello.hellospring.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.sql.DataTruncation;
 
 @Controller
 public class HelloController {
@@ -12,5 +15,11 @@ public class HelloController {
         model.addAttribute("data", "Kai!!");
         return "hello";
 
+    }
+
+    @GetMapping("hello-mvc")
+    public String helloMvc(@RequestParam("name") String name, Model model) {
+        model.addAttribute("name", name);
+        return "hello-template";
     }
 }
